c0e9ebda9d1040157363ed7e7aeeb483b61bdd16
VIJAY YADAV <01shubhamupadhyay@gmail.com>
2024-03-24 00:35:23 +0530

    Added model for predefined_exercises_table

--- GymTrackerv3/GymTracker/WorkoutTracker/models.py
+++ GymTrackerv3/GymTracker/WorkoutTracker/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 class users(models.Model):
@@ -7,4 +7,16 @@ class users(models.Model):
     username = models.CharField(max_length= 20)
     phone = models.CharField(max_length = 13)
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username + self.phone
+
+class predefined_exercises(models.Model):
     
+    class ExerciseType(models.TextChoices):
+        ISOLATION = 'isolation', _('ISOLATION EXERCISE')
+        COMPOUND = 'compound', _('COMPOUND EXERCISE')
+    id = models.AutoField(primary_key=True)
+    exercise_name = models.CharField(max_length= 50)
+    major_muscle = models.CharField(max_length=20)
+    exercise_type = models.CharField(max_length = 10, choices = ExerciseType.choices)
